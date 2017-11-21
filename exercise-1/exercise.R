1ed55e0258c85c97add3a41063c501f99e6b4cae
Farrah Lee <farrahyglee@yahoo.com>
2017-11-21 15:11:11 -0800

    Finished exercise

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -9,25 +9,39 @@ library(stringr)
 library(ggplot2)
 
 # Load Jane Austen books into a dataframe using the austen_books() function
-
+books <- austen_books()
 
 # How many books are in the dataset?
-
+num.books <- length(unique(books$book))
 
 # Which book has the most lines?
-
+most.lines <- books %>% 
+  group_by(book) %>% 
+  summarize(lines = n()) %>% 
+  filter(lines == max(lines)) %>% 
+  select(book)
 
 # Use the unnest_tokens function to generate the full list of words
-
+word.list <- books %>% unnest_tokens(word, text)
 
 # Which words are most common (regardless of which book them come from)?
-
+all.words <- word.list %>% 
+  group_by(word) %>% 
+  summarize(count = n()) %>% 
+  arrange(-count)
 
 # Remove stop words by performing an anti_join with the stop_words dataframe
-
+no.stop.words <- all.words %>% 
+  anti_join(stop_words, by="word")
 
 # Which non stop-words are most common?
-
+no.stop.words <- no.stop.words %>%
+  arrange(-count)
 
 # Use ggplot to make a horizontal bar chart of the word frequencies of non-stop words
-
+no.stop.words %>% 
+  filter(count > 800) %>% 
+  mutate(word = reorder(word, count)) %>% 
+  ggplot(aes(word, count)) +
+    geom_col() + 
+    coord_flip()
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -10,36 +10,51 @@ library(rvest)
 
 # Use the `read_html` function to load this webpage:
 # https://www.washington.edu/students/crscat/info.html
-
+webpage <- read_html('https://www.washington.edu/students/crscat/info.html')
 
 # Extract the text of each course title from the page by using the `html_nodes`
 # function to identify the *bold paragraphs*. 
 # Extract the text by passing those element to the `html_text` function
+titles <- webpage %>% html_nodes('b') %>% html_text()
 
 # Extract the *descriptions* of each course in the same process as above, searching for 
 # paragraphs (p)
+descriptions <- webpage %>% html_nodes('p') %>% html_text()
 
-
-# Create a dataframe by combinding your course titles and descriptions (skip the first description)
-
+# Create a dataframe by combining your course titles and descriptions (skip the first description)
+courses <- data.frame(title = titles, description = descriptions[2:length(descriptions)], stringsAsFactors = FALSE)
 
 # How many courses are in the catalogue?
-
+num.courses <- nrow(courses)
 
 # Create a tidytext sturcture of all words
-
+words <- courses %>% unnest_tokens(word, description)
 
 # Which words do we use to describe our courses?
-
+words.count <- words %>% 
+  group_by(word) %>% 
+  summarize(count = n())
 
 # Create a set of stop words by adding (more) irrelevant words to the stop_words dataframe
+more.words <- data.frame(
+  word = c('myplan', 'info', 'course', '5'),
+  lexicon = 'custom'
+)
 
+all.stop.words <- rbind(stop_words, more.words)
 
 # Remove stop words by performing an anti_join with the stop_words dataframe
-
+non.stop.words <- words.count %>% 
+  anti_join(all.stop.words, by="word") %>% 
+  arrange(-count)
 
 # Which non stop-words are most common?
-
+non.stop.count <- no.stop.words %>%
+  arrange(-count)
 
 # Use ggplot to make a horizontal bar chart of the word frequencies of non-stop words
-
+non.stop.words %>% 
+  filter(count > 10) %>% 
+  mutate(word = reorder(word, count)) %>% 
+  ggplot(aes(word, count)) +
+  geom_col() + coord_flip()
